033c913ed4bf6875a3ba2b102179f4f19dd16900
Don Tong <d@7fffffff.com>
2015-11-20 11:10:06 -0800

    add context menu action

--- src/background.js
+++ src/background.js
@@ -2,6 +2,7 @@ import chrome from 'chrome';
 
 const DEFAULT_SETTINGS = {
   dedup: true,
+  showContextMenuAction: true,
   priorityDomains: ['example.com'],
   blockedDomains: ['bad.example.com']
 };
@@ -18,13 +19,31 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
   if (message === 'showAction') {
     chrome.pageAction.show(sender.tab.id);
+    chrome.storage.sync.get(null, function (options) {
+      if (options.showContextMenuAction) {
+        chrome.contextMenus.create({
+          id: 'Link Grabber',
+          title: 'Link Grabber',
+          contexts: ['page'],
+        });
+      }
+    });
   }
 });
 
-chrome.pageAction.onClicked.addListener(function (tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  openLinksPage(tab);
+});
+
+chrome.pageAction.onClicked.addListener(openLinksPage);
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  delete tabData[tabId];
+});
+
+function openLinksPage (tab) {
   const linksPage = chrome.extension.getURL('html/links.html');
   const opener = tab;
-
   chrome.tabs.sendMessage(opener.id, 'getLinks', function(links) {
     chrome.tabs.create({
       index: tab.index + 1,
@@ -37,8 +56,4 @@ chrome.pageAction.onClicked.addListener(function (tab) {
       };
     });
   });
-});
-
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  delete tabData[tabId];
-});
+}
