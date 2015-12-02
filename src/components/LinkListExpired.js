4f35d9c8ec2a97f2aa1fdf38d61f9dd84d5140b0
Don Tong <d@7fffffff.com>
2015-12-01 16:00:26 -0800

    implement as functions

--- src/components/LinkListExpired.js
+++ src/components/LinkListExpired.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-const LinkListExpired = React.createClass({
-  render: function () {
-    return (
-      <div className="container-fluid">
-        <h1 className="LinkPageHeader">Expired</h1>
-        <p>
-          Link information has expired and is no longer available.
-          Please close this tab and try again.
-        </p>
-      </div>
-    );
-  }
-});
-
-export default LinkListExpired;
+export default function LinkListExpired (props) {
+  return (
+    <div className="container-fluid">
+      <h1 className="LinkPageHeader">Expired</h1>
+      <p>
+        Link information has expired and is no longer available.
+        Please close this tab and try again.
+      </p>
+    </div>
+  );
+}
